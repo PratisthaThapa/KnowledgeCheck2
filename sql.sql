cd3cb3c544f8134991cbbd299495834e218a1d84
PratisthaThapa <122835136+PratisthaThapa@users.noreply.github.com>
2023-07-16 14:11:24 -0400

    Add files via upload

---
+++ sql.sql
@@ -0,0 +1,8 @@
+﻿SELECT CustomerName FROM Orders 
+INNER JOIN Customers on Customers.CustomerID = Orders.CustomerID 
+AND Orders.OrderID = 10310 ```
+ 
+SELECT Address FROM Suppliers 
+INNER JOIN Products on Suppliers.SupplierID = Products.SupplierID 
+AND Products.ProductID = 40 
+
